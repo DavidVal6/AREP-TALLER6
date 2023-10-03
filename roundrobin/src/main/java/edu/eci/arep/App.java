74df93432c30415f20b5c6bd16a2eaabefceb484
DavidVal6 <98176834+DavidVal6@users.noreply.github.com>
2023-10-02 22:54:16 -0500

    fin sin Readme

---
+++ roundrobin/src/main/java/edu/eci/arep/App.java
@@ -0,0 +1,27 @@
+package edu.eci.arep;
+import static spark.Spark.*;
+
+import java.io.IOException;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        port(getPort());
+        staticFiles.location("/public");
+        get("hello", (req,res) -> "Hello Docker!");
+        get("/log",(req,res) -> {
+            String val = req.queryParams("value");
+            return logMessage(val);
+        });
+    }
+    private static String logMessage(String val) throws IOException {
+        //round robing
+        return HTTPConection.remoteLogCall(val);
+    }
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
